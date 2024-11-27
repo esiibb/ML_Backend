d17ee7f310b65226d2a3068815f4f0058e819f56
esiiiii <95096976+esiibb@users.noreply.github.com>
2024-11-27 19:04:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -181,6 +181,21 @@ server.route({
     },
 });
 
+server.ext('onPreResponse', (request, h) => {
+    const response = request.response;
+
+    if (response.isBoom && response.output.statusCode === 413) {
+        return h
+            .response({
+                status: 'fail',
+                message: 'Payload content length greater than maximum allowed: 1000000',
+            })
+            .code(413);
+    }
+
+    return h.continue;
+});
+
 const start = async () => {
     try {
         await loadModel();  // Memuat model ML sebelum memulai server
